4bfb54d1ae917a7c2d00190331861b453ad7142e
kyojuu <nishimiya9@yahoo.com>
2025-02-03 13:56:03 +0800

    Added Providers

---
+++ src/kojira.WebApi/Providers/Http/Response.cs
@@ -0,0 +1,15 @@
+﻿namespace kojira.WebApi.Providers.Http;
+
+public class Response
+{
+    public string? Version { get; set; }
+
+    public int StatusCode { get; set; }
+
+    public string? RequestId { get; set; }
+
+    public string? Message { get; set; }
+
+    public object? Result { get; set; }
+}
+
